ed3a70d9983c3ca539c3b3a70c5e9046029e746d
sivanesh1996 <vssivanesh@gmail.com>
2022-09-05 22:20:24 +0530

    Print fully 1 looks like rectangle

---
+++ Pattern3.java
@@ -0,0 +1,19 @@
+class Pattern3
+{
+    public static void main(String[] args)
+    {
+     int row=0;
+     while(row<3)
+     {    
+     int col=1;
+     while(col<=6)
+        {
+         System.out.print(1);
+         col++;
+        } 
+        System.out.println();
+        row++;
+     }
+    }
+}
+        
