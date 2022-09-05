6b1e7ee375ea2f4e1f3f947409230ecfa0549be3
sivanesh1996 <vssivanesh@gmail.com>
2022-09-05 22:18:27 +0530

    Print 1111 every row

---
+++ Pattern2.java
@@ -0,0 +1,19 @@
+class Pattern2
+{
+    public static void main(String[] args)
+    {
+     int row=0;
+     while(row<4)
+     {    
+     int col=1;
+     while(col<=4)
+        {
+         System.out.print(1);
+         col++;
+        } 
+        System.out.println();
+        row++;
+     }
+    }
+}
+        
