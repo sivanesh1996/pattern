fd5dd64316fc59e64117cb6bb25305072003610e
sivanesh1996 <vssivanesh@gmail.com>
2022-09-05 22:17:21 +0530

    Print 1234 every row

---
+++ Pattern1.java
@@ -0,0 +1,19 @@
+class Pattern1
+{
+    public static void main(String[] args)
+    {
+     int row=0;
+     while(row<4)
+     {    
+     int col=1;
+     while(col<=4)
+        {
+         System.out.print(col);
+         col++;
+        } 
+        System.out.println();
+        row++;
+     }
+    }
+}
+        
