d684c281b778f6bd8a6f98604163fa3989a19902
sivanesh1996 <vssivanesh@gmail.com>
2022-09-07 21:12:31 +0530

    Print 5 10 15 20 every row

---
+++ Pattern6.java
@@ -0,0 +1,42 @@
+/*class Pattern6
+{
+    public static void main(String[] args)
+    {
+     //int i=5;
+     int row=1;
+     while(row<=4)
+        {
+         int col=1;
+         int i=5;
+         while(col<=4)
+            {
+             System.out.print(i +"  ");
+             i=i+5;
+             col++;
+            }
+      System.out.println();
+      row++;
+        }
+    }
+}*/
+
+
+class Pattern6
+{
+    public static void main(String[] args)
+    {
+     //int i=5;
+     int row=1;
+     while(row<=4)
+        {
+         int i=5;
+         while(i<=20)
+            {
+             System.out.print(i +"  ");
+             i=i+5;
+            }
+      System.out.println();
+      row++;
+        }
+    }
+}
