fef192be9c7a9f4dc35e5a7699e27620a0ea528f
sivanesh1996 <vssivanesh@gmail.com>
2022-09-08 20:04:28 +0530

    print from 1 4x4

---
+++ Pattern9.java
@@ -0,0 +1,22 @@
+class Pattern9
+{
+    public static void main(String[] args)
+    {
+     int row=1;
+     int i=1;
+     while(row<=4) 
+        {
+          int col=1;
+         while(col<=4)
+            {
+            System.out.print(i +"    ");
+            i+=1;
+            col++; 
+             }
+            System.out.println(); 
+            row++;
+            }
+          
+        }
+}
+
