d42d8c66615efbd143b2f898c1c8ded72167ab0e
sivanesh1996 <vssivanesh@gmail.com>
2022-09-08 20:03:01 +0530

    print 7 left diagonal

---
+++ Pattern8.java
@@ -0,0 +1,30 @@
+class Pattern8
+{
+    public static void main(String[] args)
+    {
+     int row=1;
+   
+     while(row<=4) //statement la condition
+        {
+           int col=1;
+         while(col<=4)
+            {
+             if(row+col==5)
+                {
+                 System.out.print("7"); //statement
+                 }  
+            else
+                {
+                System.out.print("5");        
+                } 
+              col++; 
+             }
+            System.out.println(); 
+            row++;
+            }
+          
+        }
+}
+
+//devendran,siva,kavin,inba raj
+
